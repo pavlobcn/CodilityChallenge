73e04223846610f8b41f104679398a8199eefb6a
user <arskiev@gmail.com>
2018-11-16 22:36:07 +0100

    Added Node class

--- CodilityChallenge/Krypton2018/Solution.cs
+++ CodilityChallenge/Krypton2018/Solution.cs
@@ -1,14 +1,69 @@
 ﻿using System;
-// you can also use other imports, for example:
-// using System.Collections.Generic;
-
-// you can write to stdout for debugging purposes, e.g.
-// Console.WriteLine("this is a debug message");
+using System.Linq;
 
 class Solution
 {
     public int solution(int[][] A)
     {
-        return 1;
+        Node[][] matrix = GetMatrix(A);
+        Node initialNode = matrix[0][0];
+        Node pathNode = GetPath(initialNode, 0, 0, matrix);
+        return GetTrailingZeroCount(pathNode);
+    }
+
+    private Node GetPath(Node initialNode, int row, int column, Node[][] matrix)
+    {
+        return initialNode;
+    }
+
+    private Node[][] GetMatrix(int[][] initialMatrix)
+    {
+        return initialMatrix.Select(row => row.Select(GetNode).ToArray()).ToArray();
+    }
+
+    private Node GetNode(int initialValue)
+    {
+        if (initialValue == 0)
+        {
+            // special value
+            return null;
+        }
+
+        int pow2 = GetPow(2, initialValue);
+        int pow5 = GetPow(5, initialValue);
+        return new Node(pow2, pow5);
+    }
+
+    private int GetPow(int baseValue, int initialValue)
+    {
+        int result = 0;
+        while (initialValue % baseValue == 0)
+        {
+            result++;
+            initialValue /= baseValue;
+        }
+        return result;
+    }
+
+    public static int GetTrailingZeroCount(Node node)
+    {
+        return node == null ? 1 : Math.Min(node.Pow2, node.Pow5);
+    }
+
+    public class Node
+    {
+        public Node(int pow2, int pow5)
+        {
+            Pow2 = pow2;
+            Pow5 = pow5;
+        }
+
+        public int Pow2 { get; private set; }
+        public int Pow5 { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Pow2}, {Pow5}";
+        }
     }
 }
