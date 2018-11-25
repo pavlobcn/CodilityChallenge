407d7a279267b08832a468c13c1e2e1fcb9fae33
pavlobcn <arskiev@gmail.com>
2018-11-25 12:13:03 +0100

    Solution optimization

---
+++ Common/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Common
+{
+    public class BaseTest
+    {
+        protected int[] ConvertArray(string array)
+        {
+            return array.Substring(1, array.Length - 2).Split(',').Select(int.Parse).ToArray();
+        }
+
+        protected int[][] ConvertMatrix(string matrix)
+        {
+            return matrix.Substring(1, matrix.Length - 2).Replace(" ", string.Empty)
+                .Split(new[] { "]" }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(row => row.Trim(',', '[', ']').Split(',').Select(int.Parse).ToArray())
+                .ToArray();
+        }
+    }
+}
--- FutureMobility/SolutionTests.cs
+++ FutureMobility/SolutionTests.cs
@@ -1,68 +1,66 @@
-﻿using System.Linq;
+﻿using Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-[TestClass]
-public class SolutionTests
+namespace FutureMobility
 {
-    [TestMethod]
-    public void Test1()
+    [TestClass]
+    public class SolutionTests : BaseTest
     {
-        Test(
-            "[1, 1, 2, 4, 3]",
-            "[2, 2, 2, 3, 2]",
-            3);
-    }
+        [TestMethod]
+        public void Test1()
+        {
+            Test(
+                "[1, 1, 2, 4, 3]",
+                "[2, 2, 2, 3, 2]",
+                3);
+        }
 
-    [TestMethod]
-    public void Test2()
-    {
-        Test(
-            "[0, 0, 2, 1, 8, 8, 2, 0]",
-            "[8, 5, 2, 4, 0, 0, 0, 2]",
-            31);
-    }
+        [TestMethod]
+        public void Test2()
+        {
+            Test(
+                "[0, 0, 2, 1, 8, 8, 2, 0]",
+                "[8, 5, 2, 4, 0, 0, 0, 2]",
+                31);
+        }
 
-    [TestMethod]
-    public void Test3()
-    {
-        Test(
-            "[1000000000, 1000000000, 1000000000, 0, 0, 0]",
-            "[0, 0, 0, 1000000000, 1000000000, 1000000000]",
-            999999972);
-    }
+        [TestMethod]
+        public void Test3()
+        {
+            Test(
+                "[1000000000, 1000000000, 1000000000, 0, 0, 0]",
+                "[0, 0, 0, 1000000000, 1000000000, 1000000000]",
+                999999972);
+        }
 
-    [TestMethod]
-    public void Test4()
-    {
-        Test(
-            "[2]",
-            "[1]",
-            -1);
-    }
+        [TestMethod]
+        public void Test4()
+        {
+            Test(
+                "[2]",
+                "[1]",
+                -1);
+        }
 
-    [TestMethod]
-    public void TestSimple1()
-    {
-        Test(
-            "[5, 0]",
-            "[0, 5]",
-            5);
-    }
+        [TestMethod]
+        public void TestSimple1()
+        {
+            Test(
+                "[5, 0]",
+                "[0, 5]",
+                5);
+        }
 
-    private void Test(string A, string B, int expectedResult)
-    {
-        Test(Convert(A), Convert(B), expectedResult);
-    }
+        private void Test(string a, string b, int expectedResult)
+        {
+            Test(ConvertArray(a), ConvertArray(b), expectedResult);
+        }
 
-    private void Test(int[] A, int[] B, int expectedResult)
-    {
-        var solution = new Solution();
-        int actualResult = solution.solution(A, B);
-        Assert.AreEqual(expectedResult, actualResult);
-    }
-
-    private int[] Convert(string array)
-    {
-        return array.Substring(1, array.Length - 2).Split(',').Select(int.Parse).ToArray();
+        private void Test(int[] a, int[] b, int expectedResult)
+        {
+            var solution = new Solution();
+            int actualResult = solution.solution(a, b);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
