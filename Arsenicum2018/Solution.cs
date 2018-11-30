96c50d06fcf52d233f2bdfd47e001079e0ff0c98
pavlobcn <arskiev@gmail.com>
2018-11-30 19:59:25 +0100

    Added new structures

--- Arsenicum2018/Solution.cs
+++ Arsenicum2018/Solution.cs
@@ -12,9 +12,9 @@ class Solution
     {
         var root = new Node();
 
-        foreach (string word in S.Split(' '))
+        foreach (Word word in S.Split(' ').Select(w => new Word(w)))
         {
-            ProcessWord(root, word);
+            //ProcessWord(root, word);
         }
 
         var currentSentences = root.Children.Select(x => new SentenceTreeNode(x.Key, x.Value, null)).ToList();
@@ -145,4 +145,105 @@ class Solution
 
         public bool IsEndOfWord { get; set; }
     }
+}
+
+public class Word
+{
+    public Word(string word)
+    {
+        Characters = Parse(word);
+    }
+
+    private static IList<Character> Parse(string word)
+    {
+        int index = -1;
+        CharacterType type = CharacterType.Normal;
+        if (word.Length > 1)
+        {
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                // Check for EndOfHalfPalindrom
+                string leftWord = word.Substring(0, i + 1);
+                string rightWord = Reverse(word.Substring(i + 1, word.Length - i - 1));
+                if (leftWord.EndsWith(rightWord) || rightWord.EndsWith(leftWord))
+                {
+                    index = i;
+                    type = CharacterType.EndOfHalfPalindrom;
+                }
+
+                // Check for MidOfPalindrom
+                if (i == 0 || word.Length < 3)
+                {
+                    // Word is to short or start index is to small
+                    continue;
+                }
+                leftWord = word.Substring(0, i);
+                rightWord = Reverse(word.Substring(i + 1, word.Length - i - 1));
+                if (leftWord.EndsWith(rightWord) || rightWord.EndsWith(leftWord))
+                {
+                    index = i;
+                    type = CharacterType.EndOfHalfPalindrom;
+                }
+            }
+        }
+
+        var result = new List<Character>();
+        for (int i = 0; i < word.Length; i++)
+        {
+            result.Add(new Character(word[i], i == index ? type : CharacterType.Normal));
+        }
+
+        return result;
+    }
+
+    public IList<Character> Characters { get; }
+
+    public override string ToString()
+    {
+        string word = new string(Characters.Select(c => c.C).ToArray());
+        CharacterType type = CharacterType.Normal;
+        int index = -1;
+        for (int i = 0; i < Characters.Count; i++)
+        {
+            if (Characters[i].Type != CharacterType.Normal)
+            {
+                type = Characters[i].Type;
+                index = i;
+                break;
+            }
+        }
+
+        return $"{word}:{type}:{index}";
+    }
+
+    private static string Reverse(string word)
+    {
+        return new string(word.Reverse().ToArray());
+    }
+
+}
+
+public class Character
+{
+    public char C { get; }
+
+    public CharacterType Type { get; }
+
+    public Character(char c, CharacterType type)
+    {
+        C = c;
+        Type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"{C}:{Type}";
+    }
+}
+
+public enum CharacterType
+{
+    Normal,
+    MidOfPalindrom,
+    EndOfHalfPalindrom
 }
