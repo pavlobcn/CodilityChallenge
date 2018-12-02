22c9e611d6be030757ee0d4dbf01cc7846ed73d9
pavlobcn <arskiev@gmail.com>
2018-12-02 12:15:10 +0100

    Added Long String performance tests

--- Arsenicum2018/Program.cs
+++ Arsenicum2018/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main()
         {
-            new SolutionTests().TestPerformance();
+            new SolutionTests().LongStringPalindromPerformanceTest();
         }
     }
 }
--- Arsenicum2018/SolutionTests.cs
+++ Arsenicum2018/SolutionTests.cs
@@ -12,13 +12,19 @@ namespace Arsenicum2018
         private static readonly string OneLetterString2;
         private static readonly string OneLetterString3;
         private static readonly string OneLetterString4;
+        private static readonly string LongStringNoPalindromPerformance;
+        private static readonly string LongStringPalindromPerformance;
 
         static SolutionTests()
         {
             OneLetterString1 = "a";
             OneLetterString2 = "a" + Solution.Space + new string('a', 498);
             OneLetterString3 = new string('a', 498) + Solution.Space + "a";
-            OneLetterString4 = string.Join(Solution.Space.ToString(), Enumerable.Range(0, 165).Select(i => "aa")) + Solution.Space + "a" + Solution.Space + "aaa";
+            OneLetterString4 = string.Join(Solution.Space.ToString(), Enumerable.Range(0, 165).Select(i => "aa")) +
+                               Solution.Space + "a" + Solution.Space + "aaa";
+
+            LongStringNoPalindromPerformance = GetLongStringNoPalindromPerformance();
+            LongStringPalindromPerformance = GetLongStringPalindromPerformance();
         }
 
         [TestMethod]
@@ -28,6 +34,7 @@ namespace Arsenicum2018
                 "by metal owl egg mr crow worm my ate",
                 true);
         }
+
         [TestMethod]
         public void Test2()
         {
@@ -61,20 +68,6 @@ namespace Arsenicum2018
         }
 
         [TestMethod]
-        public void TestPerformance()
-        {
-            const int wordLength = 6;
-            const int wordCount = 10000;
-            var r = new Random(5);
-            var s = new string(Enumerable.Range(0, wordCount * wordLength).Select(i => (char)('a' + r.Next(26))).ToArray());
-            var words = Enumerable.Range(0, wordCount).Select(i => s.Substring(i * wordLength, wordLength)).ToList();
-
-            Test(
-                string.Join(" ", words),
-                false);
-        }
-
-        [TestMethod]
         public void OneLetterPerformanceTest1()
         {
             TestOneLetterPerformance(OneLetterString1);
@@ -96,6 +89,26 @@ namespace Arsenicum2018
         public void OneLetterPerformanceTest4()
         {
             TestOneLetterPerformance(OneLetterString4);
+        }
+
+        [TestMethod]
+        public void LongStringNoPalindromPerformanceTest()
+        {
+            const int iterationCount = 1000;
+            for (int i = 0; i < iterationCount; i++)
+            {
+                Test(LongStringNoPalindromPerformance, false);
+            }
+        }
+
+        [TestMethod]
+        public void LongStringPalindromPerformanceTest()
+        {
+            const int iterationCount = 1000;
+            for (int i = 0; i < iterationCount; i++)
+            {
+                Test(LongStringPalindromPerformance, true);
+            }
         }
 
         private void TestOneLetterPerformance(string s)
@@ -133,6 +146,48 @@ namespace Arsenicum2018
             }
 
             Assert.IsTrue(isPalindromExpected);
+        }
+
+        private static string GetLongStringNoPalindromPerformance()
+        {
+            var randomWord = new RandomWord(5);
+            string[] words = Enumerable.Range(0, 80).Select(i => randomWord.Next(3))
+                .Union(Enumerable.Range(0, 70).Select(i => randomWord.Next(4))).ToArray();
+            string[] excludePalindromes = words.Where(w => w == new string(w.Reverse().ToArray())).ToArray();
+            words = words.Where(w => !excludePalindromes.Contains(w)).ToArray();
+            string[] exclude = { "bhe", "ahb", "adh", "had", "ehc", "agc", "age", "efc",
+                "fhdf", "facd", "fgga", "dgga", "agbd", "abcb", "abcg", "bdcb", "bcag", "ghgd", "chdg", "fech", "hdga", "hadc", "gbed", "dfgc", "dfgh",
+                "cfhe", "habc"
+            };
+            words = words.Where(w => !exclude.Contains(w)).ToArray();
+            return string.Join(Solution.Space.ToString(), words);
+        }
+
+        private static string GetLongStringPalindromPerformance()
+        {
+            var randomWord = new RandomWord(5);
+            string[] words = Enumerable.Range(0, 80).Select(i => randomWord.Next(3))
+                .Union(Enumerable.Range(0, 70).Select(i => randomWord.Next(4))).ToArray();
+            string[] excludePalindromes = words.Where(w => w == new string(w.Reverse().ToArray())).ToArray();
+            words = words.Where(w => !excludePalindromes.Contains(w)).ToArray();
+            string[] exclude = {"bhe", "ahb", "adh", "had", "ehc", "agc", "age", "efc"};
+            words = words.Where(w => !exclude.Contains(w)).ToArray();
+            return string.Join(Solution.Space.ToString(), words);
+        }
+    }
+
+    class RandomWord
+    {
+        private readonly Random _random;
+
+        public RandomWord(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        public string Next(int length)
+        {
+            return new string(Enumerable.Range(0, length).Select(_ => (char) ('a' + _random.Next(8))).ToArray());
         }
     }
 }
