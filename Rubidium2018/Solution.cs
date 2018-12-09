7625140747c798038128df7b7e55a96a9c6bf815
pavlobcn <arskiev@gmail.com>
2018-12-09 13:36:33 +0100

    Initial solution

---
+++ Rubidium2018/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public int solution(int[] X, int[] Y)
+    {
+        var points = new List<Point>();
+        for (int i = 0; i < X.Length; i++)
+        {
+            points.Add(new Point(X[i], Y[i]));
+        }
+
+        int distance = int.MaxValue;
+        foreach (var pointA in points)
+        {
+            foreach (var pointB in points)
+            {
+                if (pointA == pointB)
+                {
+                    continue;
+                }
+
+                int newDistance = Math.Max(Math.Abs(pointA.X - pointB.X), Math.Abs(pointA.Y - pointB.Y));
+                if (newDistance < distance)
+                {
+                    distance = newDistance;
+                }
+
+                if (distance <= 1)
+                {
+                    return 0;
+                }
+            }
+        }
+
+        return distance / 2;
+    }
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ Rubidium2018/SolutionTests.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rubidium2018
+{
+    [TestClass]
+    public class SolutionTests : BaseTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Test(
+                "[0, 0, 10, 10]",
+                "[0, 10, 0, 10]",
+                5);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Test(
+                "[1, 1, 8]",
+                "[1, 6, 0]",
+                2);
+        }
+
+        private void Test(string xArray, string yArray, int expectedResult)
+        {
+            TestInternal(ConvertArray(xArray), ConvertArray(yArray), expectedResult);
+        }
+
+        private void TestInternal(int[] x, int[] y, int expectedResult)
+        {
+            var solution = new Solution();
+            int actualResult = solution.solution(x, y);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
