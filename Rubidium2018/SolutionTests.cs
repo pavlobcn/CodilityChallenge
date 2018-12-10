b10dee01a69e4adaae57988744ef00df0dca6fd7
pavlobcn <arskiev@gmail.com>
2018-12-10 20:49:31 +0100

    Added test

--- Rubidium2018/SolutionTests.cs
+++ Rubidium2018/SolutionTests.cs
@@ -9,13 +9,22 @@ namespace Rubidium2018
         public void Test1()
         {
             Test(
+                "[0, 2]",
+                "[0, 0]",
+                1);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Test(
                 "[0, 0, 10, 10]",
                 "[0, 10, 0, 10]",
                 5);
         }
 
         [TestMethod]
-        public void Test2()
+        public void Test3()
         {
             Test(
                 "[1, 1, 8]",
