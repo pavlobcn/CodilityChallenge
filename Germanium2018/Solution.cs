fffe9d2cfc2636a4b2784e9f3af676ca428b0a99
pavlobcn <arskiev@gmail.com>
2018-12-26 18:55:14 +0100

    Added initial solution and some tests

--- Germanium2018/Solution.cs
+++ Germanium2018/Solution.cs
@@ -1,7 +1,98 @@
-﻿class Solution
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
 {
     public int solution(int[] A, int[] B)
     {
-        return 0;
+        List<Item>[] items = GetItems(A, B);
+        int result = GetMaxNotPresent(items);
+        return result;
+    }
+
+    private int GetMaxNotPresent(List<Item>[] items)
+    {
+        for (int i = 1; i < items.Length; i++)
+        {
+            List<Item> list = items[i];
+            if (list == null || list.Count == 0)
+            {
+                return i;
+            }
+
+            if (list.Count == 1)
+            {
+                Item item = list.Single();
+                int min = Math.Min(item.A, item.B);
+                int max = Math.Max(item.A, item.B);
+                if (min == max)
+                {
+                    continue;
+                }
+
+                if (min == i)
+                {
+                    list.Remove(item);
+                    if (max < items.Length)
+                    {
+                        items[max].Remove(item);
+                    }
+                    continue;
+                }
+
+                list.Remove(item);
+                items[min].Remove(item);
+                if (items[min].Count == 0)
+                {
+                    return i;
+                }
+            }
+        }
+        return items.Length;
+    }
+
+    private List<Item>[] GetItems(int[] a, int[] b)
+    {
+        var items = new List<Item>[a.Length + 1];
+        for (int i = 0; i < a.Length; i++)
+        {
+            int valueA = a[i];
+            int valueB = b[i];
+            var item = new Item(valueA, valueB);
+            foreach (int value in new []{valueA, valueB})
+            {
+                if (value >= items.Length)
+                {
+                    continue;
+                }
+
+                List<Item> list = items[value];
+                if (list == null)
+                {
+                    list = new List<Item>();
+                    items[value] = list;
+                }
+                list.Add(item);
+            }
+        }
+        return items;
+    }
+
+    public class Item
+    {
+        public int A { get; }
+        public int B { get; }
+
+        public Item(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public override string ToString()
+        {
+            return $"{A},{B}";
+        }
     }
 }
--- Germanium2018/SolutionTests.cs
+++ Germanium2018/SolutionTests.cs
@@ -1,4 +1,6 @@
-﻿using Common;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Germanium2018
@@ -30,6 +32,92 @@ namespace Germanium2018
                 "[2, 3]",
                 "[2, 3]",
                 1);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            Test(
+                "[1, 1, 2, 3]",
+                "[1, 1, 3, 4]",
+                4);
+        }
+
+        [TestMethod]
+        public void FindBugTest()
+        {
+            foreach (int[] arrays in GetAllArrays(4, 5))
+            {
+                int[] a = arrays.Take(arrays.Length / 2).ToArray();
+                int[] b = arrays.Skip(arrays.Length / 2).ToArray();
+                int expectedResult = GetExpectedResult(a, b);
+                int actualResult = new Solution().solution(a, b);
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+        }
+
+        private IEnumerable<int[]> GetAllArrays(int length, int maxValue)
+        {
+            if (length == 0)
+            {
+                yield return new int[0];
+                yield break;
+            }
+
+            var baseArrays = GetAllArrays(length - 1, maxValue);
+            foreach (int[] baseArray in baseArrays)
+            {
+
+                for (int i = 0; i < maxValue; i++)
+                {
+                    for (int j = 0; j < maxValue; j++)
+                    {
+                        yield return baseArray.Concat(new[] {i + 1, j + 1}).ToArray();
+                    }
+                }
+            }
+        }
+
+        private int GetExpectedResult(int[] a, int[] b)
+        {
+            return GetValues(a, b).Max(GetMaxNotPresent);
+        }
+
+        private int GetMaxNotPresent(List<int> values)
+        {
+            var marks = new bool[values.Count];
+            foreach (int value in values)
+            {
+                if (value <= marks.Length)
+                {
+                    marks[value - 1] = true;
+                }
+            }
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                if (!marks[i])
+                {
+                    return i + 1;
+                }
+            }
+            return values.Count + 1;
+        }
+
+        private IEnumerable<List<int>> GetValues(int[] a, int[] b)
+        {
+            if (a.Length == 0)
+            {
+                yield return new List<int>();
+                yield break;
+            }
+
+            var baseLists = GetValues(a.Skip(1).ToArray(), b.Skip(1).ToArray()).ToList();
+            foreach (List<int> baseList in baseLists)
+            {
+                yield return new[] {a[0]}.Concat(baseList).ToList();
+                yield return new[] {b[0]}.Concat(baseList).ToList();
+            }
         }
 
         private void Test(string a, string b, int expectedResult)
