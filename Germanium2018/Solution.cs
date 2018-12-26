dfaad77452f3708ea115cd0f057f2f7c6c402981
pavlobcn <arskiev@gmail.com>
2018-12-26 12:46:46 +0100

    Added initial project for Germanium 2018

---
+++ Germanium2018/Solution.cs
@@ -0,0 +1,7 @@
+﻿class Solution
+{
+    public int solution(int[] A, int[] B)
+    {
+        return 0;
+    }
+}
---
+++ Germanium2018/SolutionTests.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Germanium2018
+{
+    [TestClass]
+    public class SolutionTests : BaseTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Test(
+                "[1, 2, 4, 3]",
+                "[1, 3, 2, 3]",
+                5);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Test(
+                "[4, 2, 1, 6, 5]",
+                "[3, 2, 1, 7, 7]",
+                4);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Test(
+                "[2, 3]",
+                "[2, 3]",
+                1);
+        }
+
+        private void Test(string a, string b, int expectedResult)
+        {
+            TestInternal(ConvertArray(a), ConvertArray(b), expectedResult);
+        }
+
+        private void TestInternal(int[] a, int[] b, int expectedResult)
+        {
+            var solution = new Solution();
+            int actualResult = solution.solution(a, b);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
