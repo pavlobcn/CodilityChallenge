81e4baa5041a0683fe9d77227aa6ddf4612f565a
pavlobcn <arskiev@gmail.com>
2019-01-20 13:24:05 +0100

    Added template for Zinc2018

---
+++ Zinc2018/Solution.cs
@@ -0,0 +1,8 @@
+﻿class Solution
+{
+    private const int Mod = 1000000007;
+    public int solution(int[] A)
+    {
+        return 0;
+    }
+}
---
+++ Zinc2018/SolutionTests.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zinc2018
+{
+    [TestClass]
+    public class SolutionTests : BaseTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Test(
+                "[1, 2, 3, 4]",
+                3);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Test(
+                "[1, 2, 3, 4]",
+                4);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Test(
+                "[2, 2, 2, 2]",
+                1);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            Test(
+                "[2, 2, 1, 2, 2]",
+                4);
+        }
+
+        private void Test(string a, int expectedResult)
+        {
+            TestInternal(ConvertArray(a) , expectedResult);
+        }
+
+        private void TestInternal(int[] a, int expectedResult)
+        {
+            var solution = new Solution();
+            int actualResult = solution.solution(a);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
