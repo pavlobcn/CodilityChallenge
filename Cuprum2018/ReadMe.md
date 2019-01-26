d2234ba7f6c43094b75399132e44ea64790f8541
pavlobcn <arskiev@gmail.com>
2019-01-26 10:20:50 +0100

    Added Cuprum 2018

---
+++ Cuprum2018/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# Challenge: 
+https://app.codility.com/programmers/challenges/cuprum2018/
+
+## Report:
+https://app.codility.com/demo/results/trainingPME3KH-T83/
---
+++ Cuprum2018/SolutionTests.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cuprum2018
+{
+    [TestClass]
+    public class SolutionTests : BaseTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            Test(
+                "6daa6ccaaa6d",
+                8);
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Test(
+                "abca",
+                0);
+        }
+
+        private void Test(string s, int expectedResult)
+        {
+            var solution = new Solution();
+            int actualResult = solution.solution(s);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
