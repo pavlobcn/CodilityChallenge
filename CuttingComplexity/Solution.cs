f411cb080ba60964c7b3c0a780133549aaf053aa
pavlobcn <arskiev@gmail.com>
2019-01-29 17:39:43 +0100

    Initial solution

--- CuttingComplexity/Solution.cs
+++ CuttingComplexity/Solution.cs
@@ -1,12 +1,220 @@
-﻿class Solution
+﻿using System;
+using System.Linq;
+
+class Solution
 {
+    private const char L = 'L';
+    private const char M = 'M';
+    private string _s;
+    private int _k;
+    private Lines _lines;
+    private int[] _splitCounts;
+    private int _totalSplitCount;
+
     public int solution(string S, int K)
     {
-        if (K == 0)
+        _s = S;
+        _k = K;
+
+        int fastResult = GetFastResult();
+        if (fastResult > -1)
+        {
+            return fastResult;
+        }
+
+        _lines = Lines.GetLines(S);
+        GetSplitCounts();
+
+        var seed = new {L = S.Take(_k).Count(c => c == L), M = S.Take(_k).Count(c => c == M), Result = _s.Length};
+        var aggregate = Enumerable.Range(0, S.Length)
+                .Aggregate(seed, (acc, i) =>
+                    {
+                        int newResult = GetSingleResult(i, acc.L, acc.M);
+                        return new {L = GetNewWeight(acc.L, i, L), M = GetNewWeight(acc.M, i, M), Result = Math.Min(acc.Result, newResult)};
+                    }
+                );
+
+        return aggregate.Result;
+    }
+
+    private void GetSplitCounts()
+    {
+        _splitCounts = new int[_s.Length];
+        _totalSplitCount = 0;
+        for (int i = 0; i < _s.Length; i++)
+        {
+            if (_s[i] == M)
+            {
+                continue;
+            }
+
+            if (i == 0)
+            {
+                continue;
+            }
+
+            if (_s[i - 1] == M)
+            {
+                int splitCount = GetSplitCount(_lines.GetLine(i - 1).Length);
+                _totalSplitCount = _totalSplitCount + splitCount;
+                _splitCounts[i] = _totalSplitCount;
+            }
+        }
+    }
+
+    private int GetNormalIndex(int index)
+    {
+        return (_s.Length + index) % _s.Length;
+    }
+
+    private int GetFastResult()
+    {
+        if (_k == 0)
         {
             return 0;
         }
 
-        return 0;
+        if (_s.All(c => c == M))
+        {
+            return GetSplitCount(_s.Length);
+        }
+
+        if (_s.All(c => c == L))
+        {
+            return _k;
+        }
+
+        return -1;
+    }
+
+    private int GetSplitCount(int lineLength)
+    {
+        return lineLength / (_k + 1);
+    }
+
+    private int GetSingleResult(int i, int l, int m)
+    {
+        int result = 0;
+        result += l;
+        int leftBound = GetNormalIndex(i - 1);
+        Line line1 = null;
+        if (_s[leftBound] == M)
+        {
+            result++;
+            line1 = _lines.GetLine(leftBound);
+            leftBound = GetNormalIndex(line1.Start - 1);
+            result += GetSplitCount(GetNormalIndex(leftBound - line1.Start));
+        }
+
+        Line line2 = null;
+        int rightBound = GetNormalIndex(i + _k);
+        if (_s[rightBound] == M)
+        {
+            result++;
+            line2 = _lines.GetLine(rightBound);
+            if (line2 == line1)
+            {
+
+            }
+            else
+            {
+                rightBound = GetNormalIndex(line2.Start + line2.Length);
+                result += GetSplitCount(GetNormalIndex(line2.Length - (rightBound - line2.Start)));
+            }
+        }
+
+        if ((line1 == null || line2 == null) && (line1 != line2))
+        {
+            result += GetSplitCountByBounds(leftBound, rightBound);
+        }
+
+        return result;
+    }
+
+    private int GetSplitCountByBounds(int leftBound, int rightBound)
+    {
+        return _totalSplitCount - (_splitCounts[rightBound] - _splitCounts[leftBound]);
+    }
+
+    private int GetNewWeight(int oldWeight, int oldPosition, char c)
+    {
+        int shift = 0;
+        if (_s[oldPosition] == c)
+        {
+            shift--;
+        }
+
+        if (_s[GetNormalIndex(oldPosition + _k)] == c)
+        {
+            shift++;
+        }
+
+        return oldWeight + shift;
+    }
+
+    public class Line
+    {
+        public int Start { get; set; }
+        public int Length { get; set; }
+
+        public override string ToString()
+        {
+            return $"Start: {Start}, Length: {Length}";
+        }
+    }
+
+    public class Lines
+    {
+        private readonly Line[] _lines;
+
+        private Lines(Line[] lines)
+        {
+            _lines = lines;
+        }
+
+        public static Lines GetLines(string s)
+        {
+            var lines = new Line[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                Line line = null;
+                char c = s[i];
+                if (c == M)
+                {
+                    if (i != 0 && s[i - 1] == M)
+                    {
+                        line = lines[i - 1];
+                        line.Length++;
+                    }
+                    else
+                    {
+                        line = new Line { Start = i, Length = 1 };
+                    }
+                }
+
+                lines[i] = line;
+            }
+
+            Line firstLine = lines[0];
+            Line lineToSubstitute = lines[lines.Length - 1];
+            if (firstLine != null && lineToSubstitute != null)
+            {
+                firstLine.Start = lineToSubstitute.Start;
+                firstLine.Length += lineToSubstitute.Length;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i] == lineToSubstitute)
+                    {
+                        lines[i] = firstLine;
+                    }
+                }
+            }
+            return new Lines(lines);
+        }
+
+        public Line GetLine(int position)
+        {
+            return _lines[position];
+        }
     }
 }
