9acf6079c3afaef86beaaa50c05a9706631900a9
pavlobcn <arskiev@gmail.com>
2019-02-02 20:26:46 +0100

    Added Nickel 2018

---
+++ Nickel2018/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# Challenge: 
+https://app.codility.com/programmers/challenges/nickel2018/
+
+## Report:
+https://app.codility.com/demo/results/training3P8KH5-69Y/
---
+++ Nickel2018/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Solution
+{
+    private const int MaxResult = 1000000000;
+
+    public int solution(bool[] P)
+    {
+        long result;
+        long total = P.LongLength * (P.LongLength + 1) / 2;
+        if (P.All(x => x))
+        {
+            result = total;
+
+        }
+        else if (P.All(x => !x))
+        {
+            return 0;
+        }
+        else
+        {
+            List<Group> groups = BuildGroups(P);
+            long groupsSum = groups.Sum(g => GetGroupValue(g));
+            result = total - groupsSum;
+        }
+
+        return (int)Math.Min(MaxResult, result);
+    }
+
+    private List<Group> BuildGroups(bool[] p)
+    {
+        var groups = new List<Group>();
+        for (int i = 0; i < p.Length; i++)
+        {
+            if (p[i])
+            {
+                continue;
+            }
+
+            if (i == 0 || p[i - 1])
+            {
+                groups.Add(new Group{Length = 1});
+            }
+            else
+            {
+                groups.Last().Length++;
+            }
+        }
+
+        return groups;
+    }
+
+    private long GetGroupValue(Group group)
+    {
+        long result = group.Length * (group.Length + 1) / 2;
+        return result;
+    }
+
+    private class Group
+    {
+        public long Length { get; set; }
+    }
+}
