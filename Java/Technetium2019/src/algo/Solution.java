3b01f4a978a995bc6bbf13aacffcddd5e645b5d9
pavlobcn <arskiev@gmail.com>
2020-02-26 20:53:29 +0100

    Technetium2019

--- Java/Technetium2019/src/algo/Solution.java
+++ Java/Technetium2019/src/algo/Solution.java
@@ -1,8 +1,69 @@
 package algo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public String solution(int[][] A)
     {
-        return "";
+        Node[][] pathNodes = new Node[A.length][A[0].length];
+        Node rootNode = goBreadth(0, 0, A, pathNodes);
+        String result = getString(rootNode);
+        return result;
+    }
+
+    private String getString(Node rootNode) {
+        StringBuffer stringBuffer = new StringBuffer();
+        while (rootNode != null)
+        {
+            stringBuffer.append(rootNode.value);
+            rootNode = rootNode.next;
+        }
+
+        return stringBuffer.toString();
+    }
+
+    private Node goBreadth(int i, int j, int[][] a, Node[][] pathNodes) {
+        if (pathNodes[i][j] != null) {
+            return pathNodes[i][j];
+        }
+        Node resultNode;
+        if (i == a.length - 1 && j == a[0].length - 1) {
+            resultNode = new Node(a[i][j], null);
+        }
+        else {
+
+            List<Node> substrings = new ArrayList();
+            if (i < a.length - 1) {
+                substrings.add(new Node(a[i][j], goBreadth(i + 1, j, a, pathNodes)));
+            }
+
+            if (j < a[0].length - 1) {
+                substrings.add(new Node((a[i][j]), goBreadth(i, j + 1, a, pathNodes)));
+            }
+
+            if (substrings.size() == 2 && substrings.get(1).value > substrings.get(0).value)
+            {
+                resultNode = substrings.get(1);
+            }
+            else
+            {
+                resultNode = substrings.get(0);
+            }
+        }
+
+        pathNodes[i][j] = resultNode;
+        return resultNode;
+    }
+
+    public static class Node
+    {
+        public int value;
+        public Node next;
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
     }
 }
