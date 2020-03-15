f4503542c313b76ba216756c1e224530fa1f2ccc
pavlobcn <arskiev@gmail.com>
2020-03-15 14:04:07 +0100

    Ruthenium 2019

--- Java/Ruthenium2019/src/algo/Solution.java
+++ Java/Ruthenium2019/src/algo/Solution.java
@@ -1,51 +1,82 @@
 package algo;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
+    int[] a;
+    int k;
+    int result = 0;
+    int startIndex = 0;
+    int endIndex = 0;
+
     public int solution(int[] A, int K) {
-        ArrayList<Integer> amounts = getAmountsToTheRight(A, K);
-        int max = getMaximum(amounts);
-        return max;
+        a  = A;
+        k = K;
+
+        for (int i = 0; i < A.length; i++) {
+            ExtendSequence();
+        }
+        return result;
     }
 
-    private ArrayList<Integer> getAmountsToTheRight(int[] a, int k) {
-        ArrayList<Integer> amounts = new ArrayList();
-        for (int i = 0; i < a.length; i++)
+    private void ExtendSequence() {
+        addNextElement();
+        if (!validate()) {
+            adjust();
+        }
+
+        result = Math.max(result, endIndex - startIndex);
+    }
+
+    private void addNextElement() {
+        endIndex++;
+    }
+
+    private boolean validate() {
+        int numberWithTheHighestEntryAmount = getNumberWithTheHighestEntryAmount();
+        int differentNumberCount = getDifferentNumberCount(a, numberWithTheHighestEntryAmount);
+        if (differentNumberCount > k)
         {
-            int amount = getSequenceToTheRightLength(i, a, k);
-            amounts.add(amount);
+            return false;
         }
-        return amounts;
+
+        return true;
     }
 
-    private int getSequenceToTheRightLength(int startIndex, int[] a, int k) {
-        int sequenceLength = a.length - startIndex;
-        int baseNumber = a[startIndex];
-        int differentNumbersCount = 0;
-        for (int i = startIndex + 1; i < a.length; i++)
+    private int getNumberWithTheHighestEntryAmount() {
+        Map<Integer,Integer> numberCountMap = new HashMap<>();
+        int numberWithTheHighestEntryAmount = -1;
+        int highestEntryAmount = 0;
+        for (int i = startIndex; i < endIndex; i++) {
+            int entryAmount = numberCountMap.containsKey(a[i]) ? numberCountMap.get(a[i]) + 1 : 1;
+            numberCountMap.put(a[i], entryAmount);
+            if (entryAmount > highestEntryAmount)
+            {
+                highestEntryAmount = entryAmount;
+                numberWithTheHighestEntryAmount = a[i];
+            }
+        }
+        return numberWithTheHighestEntryAmount;
+    }
+
+    private int getDifferentNumberCount(int[] a, int numberWithTheHighestEntryAmount) {
+        int differentNumberCount = 0;
+        for (int i = startIndex; i < endIndex; i++)
         {
-            if (a[i] == baseNumber)
+            if (a[i] != numberWithTheHighestEntryAmount)
             {
-                continue;
-            }
-
-            differentNumbersCount++;
-            if (differentNumbersCount == k + 1)
-            {
-                sequenceLength = i - startIndex;
-                break;
+                differentNumberCount++;
             }
         }
-        if (differentNumbersCount < k) {
-            // adjust by left side
-            sequenceLength += Math.min(k - differentNumbersCount, startIndex);
-        }
-        return sequenceLength;
+        return differentNumberCount;
     }
 
-    private int getMaximum(ArrayList<Integer> amounts) {
-        return Collections.max(amounts);
+    private void adjust() {
+        startIndex++;
+        if (!validate())
+        {
+            adjust();
+        }
     }
 }
