315acfaec38ab85b6c6e9535b6fcc6365bc8b1b6
pavlobcn <arskiev@gmail.com>
2020-03-19 21:15:08 +0100

    Rhodium 2019

--- Java/Rhodium2019/ReadMe.md
+++ Java/Rhodium2019/ReadMe.md
@@ -2,4 +2,4 @@
 https://app.codility.com/programmers/challenges/rhodium2019/
 
 ## Report:
-https://app.codility.com/demo/results/training3RNMHY-2KX/
+https://app.codility.com/demo/results/trainingZZR9CQ-5AV/
--- Java/Rhodium2019/src/algo/Solution.java
+++ Java/Rhodium2019/src/algo/Solution.java
@@ -1,31 +1,95 @@
 package algo;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 
 public class Solution {
     int result = 0;
+    boolean[][] missingPaths;
+    boolean[][] suspectedIncomingConnections;
+    boolean[][] suspectedOutcomingConnections;
     Node[] nodes;
 
     public int solution(int[] T) {
         nodes = prepareTree(T);
+        missingPaths = new boolean[T.length][T.length];
+        suspectedIncomingConnections = new boolean[T.length][T.length];
+        suspectedOutcomingConnections = new boolean[T.length][T.length];
         for (int i = 0; i < nodes.length; i++) {
-            findPaths(i, new HashSet());
+            checkIncoming(i);
+            checkOutcoming(i);
         }
+        checkSuspectedConnections();
+        calculateResult();
         return result;
     }
 
-    private void findPaths(int current, HashSet<Integer> edges) {
-        result++;
+    private void checkSuspectedConnections() {
+        for (int i = 0; i < missingPaths.length; i++) {
+            for (int j = i + 1; j < missingPaths.length; j++) {
+                if (suspectedIncomingConnections[i][j] && missingPaths[i][j - 1])
+                {
+                    removePaths(i, j);
+                }
+                if (suspectedOutcomingConnections[i][j] && missingPaths[i + 1][j])
+                {
+                    removePaths(i, j);
+                }
+            }
+        }
+    }
 
-        for (int i = 0; i < nodes[current].edges.size(); i++) {
-            edges.add(nodes[current].edges.get(i));
+    private void calculateResult() {
+        for (int i = 0; i < missingPaths.length; i++) {
+            for (int j = i; j < missingPaths.length; j++) {
+                if (!missingPaths[i][j])
+                {
+                    result++;
+                }
+            }
+        }
+    }
+
+    private void checkIncoming(int i) {
+        int maxIncoming = -1;
+        for (int j = 0; j < nodes[i].edges.size(); j++) {
+            if (nodes[i].edges.get(j) < i)
+            {
+                maxIncoming = Math.max(maxIncoming, nodes[i].edges.get(j));
+            }
         }
 
-        if (edges.contains(current + 1))
+        if (maxIncoming + 1 < i && maxIncoming > -1)
         {
-            findPaths(current + 1, edges);
+            suspectedIncomingConnections[maxIncoming][i] = true;
         }
+
+        for (int j = maxIncoming + 1; j < i; j++) {
+            removePaths(j, i);
+        }
+    }
+
+    private void checkOutcoming(int i) {
+        int minOutcoming = nodes.length;
+        for (int j = 0; j < nodes[i].edges.size(); j++) {
+            if (nodes[i].edges.get(j) > i)
+            {
+                minOutcoming = Math.min(minOutcoming, nodes[i].edges.get(j));
+            }
+        }
+
+        if (minOutcoming > i + 1 && minOutcoming < nodes.length)
+        {
+            suspectedOutcomingConnections[i][minOutcoming] = true;
+        }
+
+        for (int j = i + 1; j < minOutcoming; j++) {
+            removePaths(i, j);
+        }
+    }
+
+    private void removePaths(int start, int end)
+    {
+        missingPaths[start][end] = true;
     }
 
     private Node[] prepareTree(int[] t) {
