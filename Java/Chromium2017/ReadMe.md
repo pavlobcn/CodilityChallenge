d4a44ffd6756f60dddfaee5290e9b719a639feb7
pavlobcn <arskiev@gmail.com>
2020-05-07 23:23:04 +0200

    Chromium 2017

--- Java/Chromium2017/ReadMe.md
+++ Java/Chromium2017/ReadMe.md
@@ -2,5 +2,5 @@
 https://app.codility.com/programmers/task/zig_zag_escape/
 
 ## Report:
-https://app.codility.com/demo/results/trainingNQUMKP-BNN/
+https://app.codility.com/demo/results/trainingZVGU65-PW7/
 C# Report:  https://app.codility.com/demo/results/training77C62B-NGF/
--- Java/Chromium2017/src/algo/Solution.java
+++ Java/Chromium2017/src/algo/Solution.java
@@ -6,54 +6,50 @@ public class Solution {
     private final int MODULE_BASE = 1000000007;
 
     public int solution(int[] H) {
-        ArrayList<Point> points = getPoints(H);
-        sort(points);
+        int[] points = getPoints(H);
         long result = 0;
-        for (int startPointIndex = 0; startPointIndex < points.size(); startPointIndex++) {
+        for (int startPointIndex = 0; startPointIndex < points.length; startPointIndex++) {
             long pathCount = getPathCount(points, startPointIndex);
             result = (result + pathCount ) % MODULE_BASE;
         }
         return (int)(result % MODULE_BASE);
     }
 
-    private long getPathCount(ArrayList<Point> points, int startPointIndex) {
-        if (startPointIndex == points.size() - 1)
+    private long getPathCount(int[] points, int startPointIndex) {
+        if (startPointIndex == points.length - 1)
         {
             return 1;
         }
 
-        long pathCount = 1;
         long otherSidePathCount = 0;
         long sidePathCount = 1;
-        Point startPoint = points.get(startPointIndex);
-        boolean isPreviousOnTheRightSide = points.get(startPointIndex + 1).position > startPoint.position;
-        for (int i = startPointIndex + 2; i < points.size(); i++) {
-            boolean isCurrentOnTheRightSide = points.get(i).position > startPoint.position;
+        int startPoint = points[startPointIndex];
+        boolean isPreviousOnTheRightSide = points[startPointIndex + 1] > startPoint;
+        for (int i = startPointIndex + 2; i < points.length; i++) {
+            boolean isCurrentOnTheRightSide = points[i] > startPoint;
             if (isCurrentOnTheRightSide != isPreviousOnTheRightSide) {
-                // swap sides
-                long tmp = otherSidePathCount;
-                otherSidePathCount = sidePathCount;
-                sidePathCount = tmp;
+                otherSidePathCount = (otherSidePathCount + sidePathCount + 1);
+                isPreviousOnTheRightSide = !isCurrentOnTheRightSide;
             }
-
-            long pathCountToCurrentPoint = 1 + otherSidePathCount;
-            sidePathCount = (sidePathCount + pathCountToCurrentPoint);
-            pathCount = (pathCount + pathCountToCurrentPoint);
-            isPreviousOnTheRightSide = isCurrentOnTheRightSide;
+            else {
+                sidePathCount = (sidePathCount + otherSidePathCount + 1);
+                isPreviousOnTheRightSide = isCurrentOnTheRightSide;
+            }
         }
-        return pathCount + 1; // 1 - path with single start point
+        return sidePathCount + otherSidePathCount + 1; // 1 - path with single start point
     }
 
-    private void sort(ArrayList<Point> points) {
-        points.sort(Comparator.comparingInt((Point p) -> p.height));
-    }
-
-    private ArrayList<Point> getPoints(int[] h) {
+    private int[] getPoints(int[] h) {
         ArrayList<Point> points = new ArrayList<>();
         for (int i = 0; i < h.length; i++) {
             points.add(new Point(h[i], i));
         }
-        return points;
+        points.sort(Comparator.comparingInt((Point p) -> p.height));
+        int[] pointsOrderedByPosition = new int[h.length];
+        for (int i = 0; i < h.length; i++) {
+            pointsOrderedByPosition[i] = points.get(i).position;
+        }
+        return pointsOrderedByPosition;
     }
 
     private static class Point {
