693b318c7e458a5b75d8905fe4b0187d4f1f7e6e
pavlobcn <arskiev@gmail.com>
2020-05-10 18:02:22 +0200

    Chromium 2017

--- Java/Chromium2017/src/algo/Solution.java
+++ Java/Chromium2017/src/algo/Solution.java
@@ -3,77 +3,105 @@ package algo;
 import java.util.*;
 
 public class Solution {
-    private final int MODULO_BASE = 1_000_000_007;
+    private static final int MODULO_BASE = 1_000_000_007;
 
     public int solution(int[] H) {
-        Point[] points = getPoints(H);
-        int[] fastAnswers = getFastAnswers(points);
+        Point[] points = getOrderedPoints(H);
+        Point[] originalPoints = getOriginalPoints(points);
         int result = 0;
-        for (int startPointIndex = 0; startPointIndex < points.length; startPointIndex++) {
-            int pathCount = fastAnswers[startPointIndex];
-            if (pathCount == 0) {
-                pathCount = getPathCount(points, startPointIndex);
+        Point prevPoint = points[0];
+        for (int i = 1; i < points.length; i++) {
+            Point nextPoint = points[i];
+            int prevPosition = prevPoint.position;
+            int nextPosition = nextPoint.position;
+            int counterStep = prevPosition < nextPosition ? 1 : -1;
+            // skip prevPosition
+            prevPosition = prevPosition + counterStep;
+            while (prevPosition != nextPosition)
+            {
+                originalPoints[prevPosition].move(nextPoint);
+                prevPosition = prevPosition + counterStep;
             }
-            result = (result + pathCount ) % MODULO_BASE;
+            prevPoint = nextPoint;
+        }
+        for (Point point : points)
+        {
+            point.finish(points.length);
+            result = (result + point.getPathCount()) % MODULO_BASE;
         }
         return result;
     }
 
-    private int[] getFastAnswers(Point[] points) {
-        int[] fastAnswers = new int[points.length];
-        int max = points[points.length - 1].position;
-        int min = points[points.length - 1].position;
-        for (int i = points.length - 2; i >= 0 ; i--) {
-            if (points[i].position < min || points[i].position > max)
-            {
-                fastAnswers[i] = points.length - i ;
-            }
-            min = Math.min(min, points[i].position);
-            max = Math.max(max, points[i].position);
-        }
-        return fastAnswers;
-    }
-
-    private int getPathCount(Point[] points, int startPointIndex) {
-        if (startPointIndex == points.length - 1)
-        {
-            return 1;
-        }
-
-        int otherSidePathCount = 0;
-        int sidePathCount = 1;
-        Point startPoint = points[startPointIndex];
-        boolean isPreviousOnTheRightSide = points[startPointIndex + 1].position > startPoint.position;
-        for (int i = startPointIndex + 2; i < points.length; i++) {
-            boolean isCurrentOnTheRightSide = points[i].position > startPoint.position;
-            if (isCurrentOnTheRightSide != isPreviousOnTheRightSide) {
-                otherSidePathCount = (otherSidePathCount + sidePathCount + 1) % MODULO_BASE;
-                isPreviousOnTheRightSide = !isCurrentOnTheRightSide;
-            }
-            else {
-                sidePathCount = (sidePathCount + otherSidePathCount + 1) % MODULO_BASE;
-                isPreviousOnTheRightSide = isCurrentOnTheRightSide;
-            }
-        }
-        return (sidePathCount + otherSidePathCount + 1) % MODULO_BASE; // 1 - path with single start point
-    }
-
-    private Point[] getPoints(int[] h) {
+    private Point[] getOrderedPoints(int[] h) {
         Point[] points = new Point[h.length];
         for (int i = 0; i < h.length; i++) {
             points[i] = new Point(h[i], i);
         }
         Arrays.sort(points, Comparator.comparingInt((Point p) -> p.height));
+        for (int i = 0; i < h.length; i++) {
+            points[i].sortedPosition = i;
+        }
+        return points;
+    }
+
+    private Point[] getOriginalPoints(Point[] orderedPoints) {
+        Point[] points = new Point[orderedPoints.length];
+        for (Point point : orderedPoints) {
+            points[point.position] = point;
+        }
         return points;
     }
 
     private static class Point {
         public int height;
         public int position;
+        public int sortedPosition;
+        private int handledHeight;
+        private long sidePathCount;
+        private long otherSidePathCount;
 
         public Point(int height, int position) {
             this.height = height;
             this.position = position;
+        }
+
+        public int getPathCount() {
+            return (int)((otherSidePathCount + sidePathCount + 1) % Solution.MODULO_BASE);
+        }
+
+        public void move(Point nextPoint) {
+            if (height > nextPoint.height) {
+                return;
+            }
+
+            int groupSize = nextPoint.sortedPosition - sortedPosition - handledHeight - 1;
+            long pathCountToGroup = (1 + otherSidePathCount) * groupSize;
+            sidePathCount += pathCountToGroup;
+            handledHeight += groupSize;
+
+            // swap sides
+            long tmp = otherSidePathCount;
+            otherSidePathCount = sidePathCount;
+            sidePathCount = tmp;
+        }
+
+        public void finish(int lastPosition) {
+            Point finishPoint = new Point(Integer.MAX_VALUE, lastPosition);
+            finishPoint.sortedPosition = lastPosition;
+            move(finishPoint);
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "height=" + height +
+                    ", position=" + position +
+                    ", sortedPosition=" + sortedPosition +
+                    ", handledHeight=" + handledHeight +
+                    ", sidePathCount=" + sidePathCount +
+                    ", otherSidePathCount=" + otherSidePathCount +
+                    ", pathCount=" + getPathCount() +
+                    '}';
         }
     }
 }
