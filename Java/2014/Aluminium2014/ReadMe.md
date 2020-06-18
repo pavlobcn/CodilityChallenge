606b094292a8352e89b9ec1530528f697171c20e
pavlobcn <arskiev@gmail.com>
2020-06-18 23:23:22 +0200

    Aluminium2014

---
+++ Java/2014/Aluminium2014/ReadMe.md
@@ -0,0 +1,5 @@
+﻿# Challenge: 
+https://app.codility.com/programmers/challenges/aluminium2014/
+
+## Report:
+https://app.codility.com/demo/results/trainingG2QKYY-7UK/
--- Java/2014/Aluminium2014/src/tests/SolutionTest.java
+++ Java/2014/Aluminium2014/src/tests/SolutionTest.java
@@ -9,6 +9,21 @@ public class SolutionTest {
         testInternal(9, new int[]{3, 2, -6, 3, 1});
     }
 
+    @Test
+    public void test2() {
+        testInternal(-10000, new int[]{-10000});
+    }
+
+    @Test
+    public void test3() {
+        testInternal(-10000, new int[]{-10000, -10000});
+    }
+
+    @Test
+    public void test4() {
+        testInternal(4, new int[]{2, -2, -2, 2});
+    }
+
     private void testInternal(int expectedResult, int[] a) {
         var actualResult = new Solution().solution(a);
         Assert.assertEquals(expectedResult, actualResult);
