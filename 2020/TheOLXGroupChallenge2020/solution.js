c897647011afe65ad383538762c18e50198cf946
Biriukov, Pavlo <pbiriukov@netsuite.com>
2021-02-02 15:05:51 +0100

    Added OLX Group Challenge 2020

--- 2020/TheOLXGroupChallenge2020/solution.js
+++ 2020/TheOLXGroupChallenge2020/solution.js
@@ -60,3 +60,13 @@ function calculateWhenTargetCupOutList(itemsByJuice, itemsByAvailableCapacity) {
     }
     return result;
 }
+
+/* Tests */
+var result1 = solution([10, 2, 1, 1], [10, 3, 2, 2]);
+var result2 = solution([1, 2, 3, 4], [3, 6, 4, 4]);
+var result3 = solution([2, 3], [3, 4]);
+var result4 = solution([1, 1, 5], [6, 5, 8]);
+console.info("Expected: " + 2 + ". Actual: " + result1);
+console.info("Expected: " + 3 + ". Actual: " + result2);
+console.info("Expected: " + 1 + ". Actual: " + result3);
+console.info("Expected: " + 3 + ". Actual: " + result4);
