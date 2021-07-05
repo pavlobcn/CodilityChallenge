c82c059c8ebe24ba88d1edfae42de9890343c83f
pavlobcn <arskiev@gmail.com>
2021-07-05 11:58:48 +0200

    Delta 2011

--- 2011/Delta2011/ReadMe.md
+++ 2011/Delta2011/ReadMe.md
@@ -2,3 +2,4 @@
 https://app.codility.com/programmers/challenges/delta2011/
 
 ## Report:
+https://app.codility.com/demo/results/trainingNYHXMV-3QV/
--- 2011/Delta2011/src/SolutionTest.java
+++ 2011/Delta2011/src/SolutionTest.java
@@ -13,6 +13,11 @@ public class SolutionTest {
         testInternal(0, new int[]{3, 3, 3, 4, 5});
     }
 
+    @Test
+    public void test3() {
+        testInternal(0, new int[]{-50, -50, -50, -50, 4, 4, 4, 4, 18, 18, 18, 18, 99, 99, 99, 99, 100, 100, 100, 100});
+    }
+
     private void testInternal(int expectedResult, int[] a) {
         var actualResult = new Solution().solution(a);
         Assert.assertEquals(expectedResult, actualResult);
